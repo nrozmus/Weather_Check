98038f09945cf4e5fda1f5b1ba6a076c573e999a
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-12-18 22:40:11 -0500

    initial file setup.

---
+++ weather_check/lib/weather_check.rb
@@ -0,0 +1,6 @@
+require "weather_check/version"
+
+module WeatherCheck
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ weather_check/lib/weather_check/version.rb
@@ -0,0 +1,3 @@
+module WeatherCheck
+  VERSION = "0.1.0"
+end
